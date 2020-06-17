87976ce449676f61871fca1d93d38833aece1d76
Gunnar Abrahamsson <nvtesp@gmail.com>
2020-06-17 23:24:24 +0200

    removed routs and cors

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
-const cors = require('cors')
 const app = express();
 
 app.use(logger('dev'));
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
